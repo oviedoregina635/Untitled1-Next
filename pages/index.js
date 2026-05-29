027a6abd1514a2f006b35e2ca6dfb55441d3ef42
oviedoregina635 <oviedoregina635@gmail.com>
2026-05-29 17:41:50 -0600

    Commit from TeleportHQ

---
+++ pages/index.js
@@ -0,0 +1,456 @@
+import React from 'react'
+import Head from 'next/head'
+
+import { useTranslations } from 'next-intl'
+
+const LandingPage = (props) => {
+  return (
+    <>
+      <div className="landing-page-container">
+        <Head>
+          <title>exported project</title>
+          <meta property="og:title" content="exported project" />
+          <link
+            rel="canonical"
+            href="https://untitled1-gyflaz.teleporthq.site/"
+          />
+          <meta
+            property="og:url"
+            content="https://untitled1-gyflaz.teleporthq.site/"
+          />
+        </Head>
+        <div className="landing-page-thq-landing-page-elm">
+          <div className="landing-page-thq-hero-section-elm">
+            <img
+              alt="Herovideo6202"
+              src="/herovideo6202-zeip-800w.png"
+              className="landing-page-thq-herovideo-elm"
+            />
+          </div>
+          <div className="landing-page-thq-footer-elm1">
+            <div className="landing-page-thq-contactus-elm">
+              <div className="landing-page-thq-contactinformation-elm">
+                <div className="landing-page-thq-email-elm">
+                  <span className="landing-page-thq-text-elm10">
+                    enquiries@buildwithlayers.com
+                  </span>
+                </div>
+                <div className="landing-page-thq-phonenumber-elm1">
+                  <a
+                    href="https://wa.me/5214422376363?text=Hola%20quiero%20informaci%C3%B3n"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                    className="landing-page-link"
+                  >
+                    <img
+                      alt="icbaselinewhatsapp6387"
+                      src="/icbaselinewhatsapp6387-wfxw.svg"
+                      className="landing-page-thq-icbaselinewhatsapp-elm"
+                    />
+                  </a>
+                  <span className="landing-page-thq-text-elm11">
+                    +233 300 000 000
+                  </span>
+                </div>
+                <div className="landing-page-thq-location-elm">
+                  <div className="landing-page-thq-visitouroffice-elm">
+                    <span className="landing-page-thq-text-elm12">
+                      Visit our office
+                    </span>
+                    <div className="landing-page-thq-communityline-elm">
+                      <div className="landing-page-thq-group-elm1">
+                        <img
+                          alt="Vector6341"
+                          src="/vector6341-5z7j.svg"
+                          className="landing-page-thq-vector-elm1"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="landing-page-thq-address-elm1">
+                    <span className="landing-page-thq-text-elm13">
+                      8 Aburi Road, Accra Central
+                    </span>
+                    <span className="landing-page-thq-text-elm14">
+                      Accra, Ghana
+                    </span>
+                  </div>
+                </div>
+                <div className="landing-page-thq-phonenumber-elm2">
+                  <div className="landing-page-thq-frame1-elm">
+                    <span className="landing-page-thq-text-elm15">
+                      Send us a package
+                    </span>
+                    <div className="landing-page-thq-archiveline-elm">
+                      <div className="landing-page-thq-group-elm2">
+                        <img
+                          alt="Vector6351"
+                          src="/vector6351-z8f.svg"
+                          className="landing-page-thq-vector-elm2"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="landing-page-thq-address-elm2">
+                    <span className="landing-page-thq-text-elm16">
+                      P.O Box AC 0000
+                    </span>
+                    <span className="landing-page-thq-text-elm17">
+                      Accra Central, Accra
+                    </span>
+                    <span className="landing-page-thq-text-elm18">Ghana</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="landing-page-thq-footer-elm2">
+              <div className="landing-page-thq-rightside-elm1"></div>
+              <div className="landing-page-thq-rightside-elm2">
+                <span className="landing-page-thq-text-elm19">
+                  © Derechos reservados 2026
+                </span>
+              </div>
+            </div>
+          </div>
+          <img
+            alt="LOGOINTRADOS16385"
+            src="/logointrados16385-h55j-200h.png"
+            className="landing-page-thq-logointrados1-elm"
+          />
+        </div>
+      </div>
+      <style jsx>
+        {`
+          .landing-page-container {
+            width: 100%;
+            display: flex;
+            overflow: auto;
+            min-height: 100vh;
+            align-items: center;
+            flex-direction: column;
+          }
+          .landing-page-thq-landing-page-elm {
+            width: 100%;
+            height: 1261px;
+            display: flex;
+            overflow: hidden;
+            position: relative;
+            align-items: flex-start;
+            flex-shrink: 0;
+            background-color: rgba(255, 255, 255, 1);
+          }
+          .landing-page-thq-hero-section-elm {
+            top: 80px;
+            left: 732px;
+            width: 708px;
+            height: 640px;
+            display: flex;
+            position: absolute;
+            align-items: flex-start;
+            flex-shrink: 1;
+          }
+          .landing-page-thq-herovideo-elm {
+            top: 0px;
+            left: 0px;
+            width: 708px;
+            height: 640px;
+            position: absolute;
+          }
+          .landing-page-thq-footer-elm1 {
+            gap: 160px;
+            top: 936px;
+            left: 0px;
+            width: 1440px;
+            display: flex;
+            position: absolute;
+            align-items: center;
+            flex-direction: column;
+          }
+          .landing-page-thq-contactus-elm {
+            gap: 32px;
+            width: 1040px;
+            display: flex;
+            align-items: flex-start;
+            flex-shrink: 0;
+            flex-direction: column;
+          }
+          .landing-page-thq-contactinformation-elm {
+            gap: 24px;
+            width: 1040px;
+            display: flex;
+            align-items: flex-start;
+            flex-shrink: 0;
+          }
+          .landing-page-thq-email-elm {
+            gap: 16px;
+            width: 242px;
+            display: flex;
+            flex-grow: 1;
+            align-items: flex-start;
+            flex-shrink: 0;
+            flex-direction: column;
+          }
+          .landing-page-thq-text-elm10 {
+            color: rgba(0, 0, 0, 1);
+            height: auto;
+            font-size: 16px;
+            font-style: Medium;
+            text-align: left;
+            font-family: Kumbh Sans;
+            font-weight: 500;
+            line-height: 150%;
+            font-stretch: normal;
+            text-decoration: none;
+          }
+          .landing-page-thq-phonenumber-elm1 {
+            gap: 16px;
+            width: 242px;
+            display: flex;
+            flex-grow: 1;
+            align-items: flex-start;
+            flex-shrink: 0;
+            flex-direction: column;
+          }
+          .landing-page-link {
+            display: contents;
+          }
+          .landing-page-thq-icbaselinewhatsapp-elm {
+            width: 46px;
+            height: 46px;
+          }
+          .landing-page-thq-text-elm11 {
+            color: rgba(0, 0, 0, 1);
+            height: auto;
+            font-size: 16px;
+            font-style: Medium;
+            text-align: left;
+            font-family: Kumbh Sans;
+            font-weight: 500;
+            line-height: 150%;
+            font-stretch: normal;
+            text-decoration: none;
+          }
+          .landing-page-thq-location-elm {
+            gap: 16px;
+            width: 242px;
+            display: flex;
+            flex-grow: 1;
+            align-items: flex-start;
+            flex-shrink: 0;
+            flex-direction: column;
+          }
+          .landing-page-thq-visitouroffice-elm {
+            gap: 8px;
+            display: flex;
+            align-items: flex-start;
+          }
+          .landing-page-thq-text-elm12 {
+            color: var(--dl-color-primary-green);
+            height: auto;
+            font-size: 16px;
+            font-style: Medium;
+            text-align: left;
+            font-family: Kumbh Sans;
+            font-weight: 500;
+            line-height: 150%;
+            font-stretch: normal;
+            text-decoration: none;
+          }
+          .landing-page-thq-communityline-elm {
+            width: 24px;
+            height: 24px;
+            display: flex;
+            overflow: hidden;
+            position: relative;
+            align-items: flex-start;
+            flex-shrink: 0;
+          }
+          .landing-page-thq-group-elm1 {
+            top: 0px;
+            left: 0px;
+            width: 24px;
+            height: 24px;
+            display: flex;
+            position: absolute;
+            align-items: flex-start;
+            flex-shrink: 1;
+          }
+          .landing-page-thq-vector-elm1 {
+            top: 3px;
+            left: 2px;
+            width: 20px;
+            height: 18px;
+            position: absolute;
+          }
+          .landing-page-thq-address-elm1 {
+            gap: 8px;
+            display: flex;
+            align-self: stretch;
+            align-items: flex-start;
+            flex-direction: column;
+          }
+          .landing-page-thq-text-elm13 {
+            color: rgba(0, 0, 0, 1);
+            height: auto;
+            font-size: 16px;
+            font-style: Medium;
+            text-align: left;
+            font-family: Kumbh Sans;
+            font-weight: 500;
+            line-height: 150%;
+            font-stretch: normal;
+            text-decoration: none;
+          }
+          .landing-page-thq-text-elm14 {
+            color: rgba(0, 0, 0, 1);
+            height: auto;
+            font-size: 16px;
+            font-style: Medium;
+            text-align: left;
+            font-family: Kumbh Sans;
+            font-weight: 500;
+            line-height: 150%;
+            font-stretch: normal;
+            text-decoration: none;
+          }
+          .landing-page-thq-phonenumber-elm2 {
+            gap: 16px;
+            width: 242px;
+            display: flex;
+            flex-grow: 1;
+            align-items: flex-start;
+            flex-shrink: 0;
+            flex-direction: column;
+          }
+          .landing-page-thq-frame1-elm {
+            gap: 8px;
+            display: flex;
+            align-items: flex-start;
+          }
+          .landing-page-thq-text-elm15 {
+            color: var(--dl-color-primary-green);
+            height: auto;
+            font-size: 16px;
+            font-style: Medium;
+            text-align: left;
+            font-family: Kumbh Sans;
+            font-weight: 500;
+            line-height: 150%;
+            font-stretch: normal;
+            text-decoration: none;
+          }
+          .landing-page-thq-archiveline-elm {
+            width: 24px;
+            height: 24px;
+            display: flex;
+            overflow: hidden;
+            position: relative;
+            align-items: flex-start;
+            flex-shrink: 0;
+          }
+          .landing-page-thq-group-elm2 {
+            top: 0px;
+            left: 0px;
+            width: 24px;
+            height: 24px;
+            display: flex;
+            position: absolute;
+            align-items: flex-start;
+            flex-shrink: 1;
+          }
+          .landing-page-thq-vector-elm2 {
+            top: 2.999997854232788px;
+            left: 2px;
+            width: 20px;
+            height: 18px;
+            position: absolute;
+          }
+          .landing-page-thq-address-elm2 {
+            gap: 8px;
+            display: flex;
+            align-self: stretch;
+            align-items: flex-start;
+            flex-direction: column;
+          }
+          .landing-page-thq-text-elm16 {
+            color: rgba(0, 0, 0, 1);
+            height: auto;
+            font-size: 16px;
+            font-style: Medium;
+            text-align: left;
+            font-family: Kumbh Sans;
+            font-weight: 500;
+            line-height: 150%;
+            font-stretch: normal;
+            text-decoration: none;
+          }
+          .landing-page-thq-text-elm17 {
+            color: rgba(0, 0, 0, 1);
+            height: auto;
+            font-size: 16px;
+            font-style: Medium;
+            text-align: left;
+            font-family: Kumbh Sans;
+            font-weight: 500;
+            line-height: 150%;
+            font-stretch: normal;
+            text-decoration: none;
+          }
+          .landing-page-thq-text-elm18 {
+            color: rgba(0, 0, 0, 1);
+            height: auto;
+            font-size: 16px;
+            font-style: Medium;
+            text-align: left;
+            font-family: Kumbh Sans;
+            font-weight: 500;
+            line-height: 150%;
+            font-stretch: normal;
+            text-decoration: none;
+          }
+          .landing-page-thq-footer-elm2 {
+            gap: 286px;
+            width: 1440px;
+            display: flex;
+            padding: 0 200px 24px;
+            align-items: center;
+            flex-shrink: 0;
+            justify-content: space-between;
+            background-color: rgba(255, 255, 255, 1);
+          }
+          .landing-page-thq-rightside-elm1 {
+            gap: 24px;
+            display: flex;
+            align-items: flex-start;
+          }
+          .landing-page-thq-rightside-elm2 {
+            gap: 24px;
+            display: flex;
+            align-items: flex-start;
+          }
+          .landing-page-thq-text-elm19 {
+            color: rgba(102, 102, 102, 1);
+            height: auto;
+            font-size: 16px;
+            font-style: Medium;
+            text-align: left;
+            font-family: Kumbh Sans;
+            font-weight: 500;
+            line-height: 150%;
+            font-stretch: normal;
+            text-decoration: none;
+          }
+          .landing-page-thq-logointrados1-elm {
+            top: 327px;
+            left: 141px;
+            width: 494px;
+            height: 146px;
+            position: absolute;
+          }
+        `}
+      </style>
+    </>
+  )
+}
+
+export default LandingPage
